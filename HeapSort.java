bc3e85a9fc6f0bb9a439ccb0c2f885ac89dffcb2
Yunpeng Gao <yg6c2@mst.edu>
2017-02-08 21:06:21 -0600

    m

--- HeapSort.java
+++ HeapSort.java
@@ -3,13 +3,15 @@ package collection;
 public class HeapSort {
 
 	public static void main(String[] args) {
-		int[] array = { -22, 0, 11, 1, 88, 19, -5,-1 };
+		int[] array = { -1, 10, 2, 5, 6, 1, 0 };
 		Heap heap = new Heap();
 		for (int x : array) {
 			heap.addItem(x);
 		}
 		// heap.getList();
 
+		heap.getList();
+		System.out.println();
 		for (int i = array.length - 1; i >= 0; i--) {
 			array[i] = heap.removeItem();
 		}
